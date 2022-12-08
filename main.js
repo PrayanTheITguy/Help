2e39983ac6e6858a44d3d49f9d90f9ad5cc453af
PrayanTheITguy <113575293+PrayanTheITguy@users.noreply.github.com>
2022-12-08 10:19:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+Webcam.set({
+    width:300,
+    height:250,
+    image_format : 'png',
+    png_quality:90
+  });
+
+  camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+      
+
